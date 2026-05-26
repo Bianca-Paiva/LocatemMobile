9315ba24702e5760e7744b597b353b2ac33f77c5
Bianca <biancasilveriapaivarg@gmail.com.>
2026-05-26 14:31:11 -0300

    adiciona telas de Cadastro e Login, e configurações de rotas

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,10 @@
+import { createStackNavigator } from "@react-navigation/stack"; 
+const Stack = createStackNavigator();
+
+export default function AppRoutes() {
+    return (
+        <Stack.Navigator>
+            
+        </Stack.Navigator>
+    )
+}
