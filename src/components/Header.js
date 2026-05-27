fd5420e4513fd97ef171161d7e1168bc786b9cf9
BIANCA SILVERIA PAIVA <bianca.spaiva@senacsp.edu.br>
2026-05-27 19:51:29 -0300

    adiciona componente Header e tela de Cadastro, atualiza dependências no package.json

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default function Header() {
+    return (
+        <LinearGradient
+            colors={["#FFD600", "#F2CB00", "#FFF6C7", "#F9FAFB"]}
+            locations={[0, 0.3, 0.75, 1]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 0, y: 1 }}
+            style={styles.headerContainer}
+        >
+            <Image
+                source={require('../image/LogoLocatem.png')}
+                style={styles.logoImg}
+            />
+            <Text style={styles.logoTexto}>
+                LOCATEM
+            </Text>
+        </LinearGradient>
+    );
+}
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        width: "100%",
+        height: 140, // altura do header
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 20,
+        gap: 10,
+    },
+
+    logoImg: {
+        width: 35,
+        height: 35,
+        resizeMode: "contain", // mantém proporção da imagem
+    },
+
+    logoTexto: {
+        fontSize: 27,
+        fontWeight: "800",
+        color: "#000",
+        letterSpacing: 1,
+    },
+})
